b65d35cdd5e4a10b7d1cf7797b9398e43e710da2
usuario <usuario@10.22.143.30>
2015-05-15 11:25:00 +0100

    Primer commit

---
+++ src/footballpool/FootballPool.java
@@ -0,0 +1,36 @@
+package footballpool;
+
+import eu.dataaccess.footballpool.ArrayOfString;
+
+public class FootballPool {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+    }
+
+    public static ArrayOfString allGoalKeepers(java.lang.String sCountryName) {
+        eu.dataaccess.footballpool.Info service = new eu.dataaccess.footballpool.Info();
+        eu.dataaccess.footballpool.InfoSoapType port = service.getInfoSoap();
+        return port.allGoalKeepers(sCountryName);
+    }
+    
+    public static ArrayOfString allDefenders(java.lang.String sCountryName) {
+        eu.dataaccess.footballpool.Info service = new eu.dataaccess.footballpool.Info();
+        eu.dataaccess.footballpool.InfoSoapType port = service.getInfoSoap();
+        return port.allDefenders(sCountryName);
+    }
+
+    public static ArrayOfString allMidFields(java.lang.String sCountryName) {
+        eu.dataaccess.footballpool.Info service = new eu.dataaccess.footballpool.Info();
+        eu.dataaccess.footballpool.InfoSoapType port = service.getInfoSoap();
+        return port.allMidFields(sCountryName);
+    }
+
+    public static ArrayOfString allForwards(java.lang.String sCountryName) {
+        eu.dataaccess.footballpool.Info service = new eu.dataaccess.footballpool.Info();
+        eu.dataaccess.footballpool.InfoSoapType port = service.getInfoSoap();
+        return port.allForwards(sCountryName);
+    }
+
+}
